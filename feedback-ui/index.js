9445a77fbea6ceccc879eef476a22aae5ff76dac
Maverick181 <105151056+Maverick181@users.noreply.github.com>
2023-05-11 15:23:46 +0530

    Add files via upload

---
+++ feedback-ui/index.js
@@ -0,0 +1,31 @@
+let selectedRating = "";
+const ratingEls = document.querySelectorAll(".rating");
+const btnEl = document.getElementById("btn");
+const containerEl = document.getElementById("container");
+
+ratingEls.forEach((ratingEl)=>{
+    ratingEl.addEventListener("click",(event)=>{
+        removeActive();
+        selectedRating = event.target.innerText || event.target.parentNode.innerText;
+        event.target.classList.add("active");
+        event.target.parentNode.classList.add("active");
+    });
+});
+
+btn.addEventListener("click",(event)=>{
+    if(selectedRating !== ""){
+        containerEl.innerHTML =`
+            <strong>Thank You !!</strong>
+            <br><br>
+            <strong>Feedback: ${selectedRating}</strong>
+            <br>
+            <p>We'll use your feedback to improve our customer support. </p>
+        `;
+    }
+});
+
+function removeActive(){
+    ratingEls.forEach((ratingEl)=>{
+        ratingEl.classList.remove("active");
+    });
+} 
